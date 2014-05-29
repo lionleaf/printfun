1904515a1d5ef78b881f315fdaf8d40d3b83bc36
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-05-29 22:19:18 +0200

    make a test print file

--- js/printgen.js
+++ js/printgen.js
@@ -1,34 +1,69 @@
 var printfile = [];
 
+var dir = 1;
+
+
+//Initial move:
+var movecmd ={
+    "command" : 
+    {
+        "function" : "move",
+        "metadata" : 
+        {
+            "relative" : 
+            {
+                "a" : false,
+                "x" : false,
+                "y" : false,
+                "z" : false
+            }
+        },
+        "parameters" : 
+        {
+            "a" : 0.0,
+            "feedrate" : 100,
+            "x" : -80,
+            "y" : -80.0,
+            "z" : 0.0
+        },
+        "tags" : [ "Travel move" ]
+    }
+} 
+
+dir *= -1; 
+printfile.push(movecmd);
+
+
 
 for(var i = 0; i < 15; i++){
-    var movecmd ={
-        "command" : 
-        {
-            "function" : "move",
-            "metadata" : 
+        var movecmd ={
+            "command" : 
             {
-                "relative" : 
+                "function" : "move",
+                "metadata" : 
                 {
-                    "a" : false,
-                    "x" : false,
-                    "y" : false,
-                    "z" : false
-                }
-            },
-            "parameters" : 
-            {
-                "a" : 0.0,
-                "feedrate" : 10.0 + 10.0*i,
-                "x" : -75.0 + 10 * i,
-                "y" : 0.0,
-                "z" : 0.0
-            },
-            "tags" : [ "Travel move" ]
-        }
-    } 
+                    "relative" : 
+                    {
+                        "a" : false,
+                        "x" : false,
+                        "y" : false,
+                        "z" : false
+                    }
+                },
+                "parameters" : 
+                {
+                    "a" : 0.0,
+                    "feedrate" : 20.0 + 10.0*i,
+                    "x" : dir*80,
+                    "y" : dir*80,
+                    "z" : 0.0
+                },
+                "tags" : [ "Travel move" ]
+            }
+        } 
 
-    printfile.push(movecmd);
+        dir *= -1; 
+        printfile.push(movecmd);
 }
 
 
